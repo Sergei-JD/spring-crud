513c715adc1a83817e60209eb7607afaefe55b5d
Sergei-JD <sergshlyazhko@gmail.com>
2022-03-24 10:13:55 +0400

    SQL injection. PreparedStatement. JDBC API

--- src/main/java/com/spring/crud/controllers/PeopleController.java
+++ src/main/java/com/spring/crud/controllers/PeopleController.java
@@ -1,12 +1,18 @@
 package com.spring.crud.controllers;
 
+import com.spring.crud.dao.PersonDAO;
 import com.spring.crud.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import com.spring.crud.dao.PersonDAO;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
